33d1d9ac672587ec60c75e92d994986f207c302c
Gopal-nd <gopalhelpyou@gmail.com>
2024-12-13 21:12:24 +0530

    profile page and color theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,11 @@ import SignUpPage from "./pages/SignUpPage";
 import { axiosInstance } from "./lib/axios";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+const {theme} = useThemeStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -27,7 +29,7 @@ const App = () => {
     );
   }
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route

555a5e02e8fdfd9683b6336f85ba42edb2bb7456
Gopal-nd <gopalhelpyou@gmail.com>
2024-12-14 14:56:26 +0530

    typo fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,13 +33,13 @@ app.use("/api/messages",messageRoutes);
 
 
 
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 

c70b51f717e8fedaeab1105e872be484268c3d89
Gopal-nd <gopalhelpyou@gmail.com>
2024-12-14 14:36:45 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,13 @@ const PORT = process.env.PORT;
 
 app.use("/api/auth",authRouter);
 app.use("/api/messages",messageRoutes);
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
+app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
+}
 server.listen(PORT,()=>{
   connectDB();
   console.log("server is running on port : "+PORT)
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -58,6 +58,8 @@ if(!selectedUser) return;
 const socket = useAuthStore.getState().socket;
 
 socket.on("newMessage",(newMessage)=>{
+const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+if(!isMessageSentFromSelectedUser) return;
 set({
 messages:[...get().messages,newMessage]
 })
